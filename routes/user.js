841ffb9b483beec9977da9562a320135184a06f3
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-17 12:05:09 -0500

    search database for existing user before saving

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {User} = require("../database/models");
 const bcrypt = require("bcrypt");
+const Sequelize = require('sequelize');
 
 // Find all of the games of the season, with the homeTeam and awayTeam eager loaded;
 router.post('/register', (req, res, next) => {
@@ -10,21 +11,34 @@ router.post('/register', (req, res, next) => {
 
     let newUser = User.build(req.body)
 
-    //search db to see if the user already exists
-    
-    newUser.email = newUser.email.toLowerCase();
+    User.findOne({
+        where: Sequelize.or (
+            {email: req.body.email},
+            {username: req.body.username}
+        )
+    })
+    .then(user => {
 
-    // Hash password before saving in database
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-            .save()
-            .then(user => res.json(user))
-            .catch(err => next(err));
+        if(user) {
+            return res.status(400).json("User already exists");
+        }
+
+        newUser.email = newUser.email.toLowerCase();
+
+        // Hash password before saving in database
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser
+                .save()
+                .then(user => res.status(200).json(user))
+
+                //TODO: Improve error handling
+                .catch(err => next(err));
+            });
         });
-    });
+    })
 
 });
 
