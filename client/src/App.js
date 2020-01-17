d1d0fa37535bccb771dca9a8feb03832408e7585
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-17 13:11:18 -0500

    added redux and made register page on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Register from "./components/auth/Register";
+import { Provider } from "react-redux";
+import store from "./store"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                        <div className="App">
+                            <Switch>
+                                <Route exact path="/" component={Register} />
+                            </Switch>
+                        </div>
+                    </Router>
+            </Provider>
+          );
+    }
+
 }
 
 export default App;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+export default class Register extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state ={
+            email: "",
+            username: "",
+            password1: "'",
+            password2: ""
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Register</h1>
+                <br></br>
+                <br></br>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <input id="email" onChange={this.handleChange} value={this.state.email}></input>
+                    </div>
+                    <div>
+                        <input id="username" onChange={this.handleChange} value={this.state.username}></input>
+                    </div>
+                    <div>
+                        <input id="password1" onChange={this.handleChange} value={this.state.password1}></input>
+                    </div>
+                    <div>
+                        <input id="password2" onChange={this.handleChange} value={this.state.password2}></input>
+                    </div>
+                    <div>
+                        <button type="submit" >Register</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
