a23012fdb25e50885017fee158c40f74d102f9cc
Khinshan Khan <khanshan501@gmail.com>
2020-01-18 17:44:57 -0500

    added time based logic for turns later

--- sockets/index.js
+++ sockets/index.js
@@ -1,19 +1,30 @@
-const emitter = socket => {
-  return io.emit("FromAPI", players);
-};
+// declare interval for server based turn logic
+let interval;
 
+// keep an array of
+// {token id: socket id}
 let users = [];
+// keep a list of moves made
 let moves = [];
+
+// logic for when the time interval has completed
+const emitter = socket => {
+  console.log(`it is time: ${Date.now()}`);
+  return "hi"; //io.emit("FromAPI", players);
+};
 
 module.exports = io => {
   io.on("connection", socket => {
     console.log("New client connected");
     io.emit("sent2", { moves: moves });
 
-    // if (interval) {
-    //   clearInterval(interval);
-    // }
-    // interval = setInterval(() => emitter(socket), 10);
+    // reset timer
+    if (interval) {
+      clearInterval(interval);
+    }
+
+    // print message every 60 seconds
+    interval = setInterval(() => emitter(socket), 60000);
 
     socket.on("token", function(data) {
       let obj = {};
@@ -22,7 +33,7 @@ module.exports = io => {
     });
 
     socket.on("sendy", function(data) {
-      console.log(data);
+      // console.log(data);
       moves.push(data);
       io.emit("sent", { moves: moves });
     });
