b00ebfb5aabefd5e0787474205b9e9e63f9cf8c1
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-21 16:02:37 -0500

    fixed file structure

--- client/src/components/drawing/CanvasContainer.js
+++ client/src/components/drawing/CanvasContainer.js
@@ -6,6 +6,12 @@ import FormControl from "@material-ui/core/FormControl";
 import Timer from "./Timer";
 import NativeSelect from "@material-ui/core/NativeSelect";
 import Button from "@material-ui/core/Button";
+import TextInput from "./TextInput.js";
+
+const divStyle = {
+  display: "flex",
+  alignItems: "center"
+};
 
 class CanvasContainer extends Component {
   constructor(props) {
@@ -169,12 +175,12 @@ class CanvasContainer extends Component {
 
   determineDisplay = () => {
     if (this.state.turn) {
-      return <div>
+      return (
+        <div>
           <h1>It's your turn.</h1>
-          <p>
-            Your word is: {this.state.currWord}
-          </p>
-          </div>;
+          <p>Your word is: {this.state.currWord}</p>
+        </div>
+      );
     } else {
       return <div>Wait for your turn...</div>;
     }
@@ -225,16 +231,23 @@ class CanvasContainer extends Component {
           </Button>
         </div>
         <br></br>
-        <canvas
-          style={{ background: "gray" }}
-          onMouseDown={this.onMouseDown}
-          onMouseLeave={this.endPaintEvent}
-          onMouseUp={this.endPaintEvent}
-          onMouseMove={this.onMouseMove}
-          ref="canvas"
-          width={950}
-          height={600}
-        />
+        <div style={divStyle}>
+          <canvas
+            style={{
+              background: "gray",
+              paddingRight: "30%",
+              marginRight: "3%"
+            }}
+            onMouseDown={this.onMouseDown}
+            onMouseLeave={this.endPaintEvent}
+            onMouseUp={this.endPaintEvent}
+            onMouseMove={this.onMouseMove}
+            ref="canvas"
+            width={950}
+            height={600}
+          />
+          <TextInput />
+        </div>
       </div>
     );
   }
--- client/src/components/drawing/GameView.js
+++ client/src/components/drawing/GameView.js
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
 import CanvasContainer from "./CanvasContainer.js";
-import TextInput from "./TextInput.js";
 
 class GameView extends Component {
   render() {
     return (
       <div className="game">
-        <div className="gamealign">
-        </div>
+        <div className="gamealign"></div>
         <div className="alignment">
           <div className="paper">
             <CanvasContainer />
-          </div>
-          <div className="paper2">
-            <TextInput />
           </div>
         </div>
       </div>
