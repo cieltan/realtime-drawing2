a97a4a51b37e1d7dfc350a13b155bce9cba198ba
btan <btan7645@gmail.com>
2020-01-23 17:46:15 -0500

    Deploying

---
+++ client/build/precache-manifest.bd8faafb61f4480b960f461bfe319f56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2158556349cdf01b3c65069dfadb0297",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dc2e0ef3146547ca44e",
+    "url": "/static/css/main.45311841.chunk.css"
+  },
+  {
+    "revision": "d93a325d7b18c5c2cbab",
+    "url": "/static/js/2.5e9f516e.chunk.js"
+  },
+  {
+    "revision": "a068657e4f451d28c30deb405f0ebadc",
+    "url": "/static/js/2.5e9f516e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5dc2e0ef3146547ca44e",
+    "url": "/static/js/main.2b9efa38.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
