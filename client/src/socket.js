4e56726396bda23242dc747f6dfe227c3ea61a9c
btan <btan7645@gmail.com>
2020-01-24 00:00:22 -0500

    Server init changed

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io('window.location.origin')
+const socket = io(window.location.origin)
 
 socket.on('connect', () => {
   console.log('Connected!')
